ea116056a5c8cf42b152695e271430f7380f2022
Kumac13 <foohilack@gmail.com>
2023-11-22 08:26:18 +0900

    feat: set skelton

---
+++ denops/sf/main.ts
@@ -0,0 +1,18 @@
+import {
+  Denops,
+  ensureString,
+} from './deps.ts';
+
+export async function main(denops: Denops): Promise<void> {
+  denops.dispatcher = {
+    async echo(text: unknown): Promise<unknown> {
+      ensureString(text);
+      return await Promise.resolve(text);
+    },
+  };
+
+  await denops.cmd(
+    `command! -nargs=1 HelloWorldEcho echomsg denops#request('${denops.name}', 'echo', [<q-args>])`,
+  );
+};
+
